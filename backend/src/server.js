8cf84a60cfb2bde0ec9fd97f227eb74e86919ab1
Larissa Georgina <larissa.gcrs@gmail.com>
2020-01-08 11:44:02 -0200

    Criando Model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
+mongoose.connect('mongodb+srv://omni:omni@cluster0-fwrdb.mongodb.net/omnistack8?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+server.use(express.json());
 server.use(routes);
 server.listen(3333);
