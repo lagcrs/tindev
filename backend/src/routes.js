949e6c0c243733e4956ccbf118973d99340c0f58
Larissa Georgina <larissa.gcrs@gmail.com>
2020-01-08 00:57:01 -0200

    Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
 const routes = express.Router();
+
 routes.get('/', (req, res) => {
     return res.send('Hello Larissa')
+});
+
+routes.post('/devs', (req, res) => {
+    return res.json({ ok: true })
 });
 
 module.exports = routes;
